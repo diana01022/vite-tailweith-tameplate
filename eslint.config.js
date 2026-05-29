316a57d41d2078e0bc4e81d5ba601a2725304c63
Diana <dianaboguckaa8@gmail.com>
2026-05-29 13:32:48 +0300

    Lisa ESlint

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+    js.configs.recommended,
+    eslintConfigPrettier,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+    },
+    {
+        ignores: ["dist/", "node_modules/"],
+    }
+]
